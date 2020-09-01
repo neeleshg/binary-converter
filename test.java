33c9ff8486634e94a14e82f90087ecb042a4c7d9
Neelesh <ngurjar@neeleshgurjar.co.in>
2020-09-01 08:32:30 +0530

    added test.java

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.awt.imageComponentColorModel;
+public class b
+  extends ComponentColorModel
+{
+ public b()
+ {
+  super(new a), new int[] { 8, 8, 8 }, false, false, 1, 0);
+ }
+ public boolean isCompatibleRaster(Raster paramRaster)
+  {
+   return true;
+  }
+
+public class a
+ extends ICC_Colorspace
+{
+public a()
+ }
+  super(g.a());
+ }
+public int getNumComponents()
+ {
+  return l;
+ }
